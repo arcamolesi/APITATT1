818192ad9219203b29e01a12d75dbc1cce152c03
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-02-18 18:02:07 -0300

    Atualização API TAT1 Disciplinas

---
+++ Model/Disciplina.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITATT1.Model
+{
+    public class Disciplina
+    {
+        [Key]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        public string nome { get; set; }
+    }
+}
