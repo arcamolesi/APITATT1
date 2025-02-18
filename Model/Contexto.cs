8c4217e85c0937996249f388ea05351be066a8d4
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-02-18 18:02:07 -0300

    Add project files.

---
+++ Model/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APITATT1.Model
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+    }
+}
